05d849724d929b0a1c8e5deca2edf86a24ea784d
Kushagra9399 <146541055+Kushagra9399@users.noreply.github.com>
2025-02-09 22:58:53 +0530

    Add files via upload

---
+++ image_recoginition_using_ai.py
@@ -0,0 +1,13 @@
+from roboflow import Roboflow
+
+# Initialize the Roboflow API with your API key
+rf = Roboflow(api_key="6dppQzzindtJMxDv1DjW")
+
+# Choose your workspace and project name (replace these with your actual workspace and project name)
+workspace = "YOUR_WORKSPACE_NAME"
+project_name = "YOUR_PROJECT_NAME"
+
+# Get the dataset (replace 'version' with your dataset version number)
+dataset = rf.workspace("ambulancedetection-td1oy").project("ambulance-dn9qr").version(4).download("yolov8")  # Change '1' to your version number
+
+print(f"Dataset downloaded to: {dataset.location}")
