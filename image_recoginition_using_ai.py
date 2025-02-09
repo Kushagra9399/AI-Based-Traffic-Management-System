07b92ca7b5b184264069da670707fa4165afebdd
Kushagra9399 <146541055+Kushagra9399@users.noreply.github.com>
2025-02-09 22:59:26 +0530

    Update image_recoginition_using_ai.py

--- image_recoginition_using_ai.py
+++ image_recoginition_using_ai.py
@@ -1,7 +1,7 @@
 from roboflow import Roboflow
 
 # Initialize the Roboflow API with your API key
-rf = Roboflow(api_key="6dppQzzindtJMxDv1DjW")
+rf = Roboflow(api_key="API KEY")
 
 # Choose your workspace and project name (replace these with your actual workspace and project name)
 workspace = "YOUR_WORKSPACE_NAME"
